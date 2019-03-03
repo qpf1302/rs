152f4d576a2c42b5bbc4694bd515edec17c07a0c
qpf1302 <609717927@qq.com>
2019-03-04 00:52:44 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
  'DB_TYPE'   => 'mysql', // 数据库类型
- 'DB_HOST'   => 'localhost', // 服务器地址
- 'DB_NAME'   => 'rs_xidian_cc', // 数据库名monitor
- 'DB_USER'   => 'root', // 用户名
- 'DB_PWD'    => 'root', // 密码See@2012
+ 'DB_HOST'   => '', // 服务器地址
+ 'DB_NAME'   => '', // 数据库名monitor
+ 'DB_USER'   => '', // 用户名
+ 'DB_PWD'    => '', // 密码See@2012
  'DB_PORT'   => 3306, // 端口
  'DB_PREFIX' => 'rs_', // 数据库表前缀
  'DB_CHARSET'=> 'utf8', // 字符集
